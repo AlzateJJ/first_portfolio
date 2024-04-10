5fc14a3236439d41337454749a5e9b82f7f5a2a8
AlzateJJ <124931160+AlzateJJ@users.noreply.github.com>
2024-04-10 12:47:47 -0500

    fixing navbar_effect

--- assets/js/components/selected_menu.js
+++ assets/js/components/selected_menu.js
@@ -1,11 +1,11 @@
-const navbarListDOM = document.querySelector('.navbar_list')
+const navbarListDOM = document.querySelector('.navbar__list')
 const navbarLinksDOM = document.querySelectorAll('.navbar__link')
 const sectionsDOM = document.querySelectorAll('section')
 
 function activeMenu(e) {
     if (e && e.type === 'click') {
-        const clickedLink = e.target.closests('navbar__link')
-        const activeLink = navbarListDOM.querySelector('.navbar__Link.active')
+        const clickedLink = e.target.closest('navbar__link')
+        const activeLink = navbarListDOM.querySelector('.navbar__link.active')
 
         if (activeLink) {
             activeLink.classList.remove('active')
@@ -15,11 +15,12 @@ function activeMenu(e) {
             clickedLink.classList.add('active')
         }
     } else if (e && e.type === 'scroll'){
+
         const scrollPosition = window.scrollY + 50
 
         for (let i = sectionsDOM.length - 1; i >= 0; i--) {
             if (scrollPosition >= sectionsDOM[i].offsetTop) {
-                const activeLink = navbarListDOM.querySelector('.navbar__Link.active')
+                const activeLink = navbarListDOM.querySelector('.navbar__link.active')
 
                 if (activeLink) {
                     activeLink.classList.remove('active')
