5de53414b867e731a102d2948a63434adcc9a877
AlzateJJ <124931160+AlzateJJ@users.noreply.github.com>
2024-04-10 16:32:29 -0500

    fixing other stuff

--- assets/js/components/parallax.js
+++ assets/js/components/parallax.js
@@ -5,8 +5,10 @@ function parallax(e) {
     if (e) {
         imageDom.forEach(img => {
             const speed = img.getAttribute('data-speed')
+            // console.log(img.getAttribute('data-speed'))
             const x = (window.innerWidth - e.pageX * speed) / 100
             const y = (window.innerHeight - e.pageY * speed) / 100
+            console.log(`x=${x}, y=${y}`)
 
             img.style.transform = `translateX(${x}px translateY(${y}px))`
         })
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 import parallax from "./components/parallax.js";
 import activeMenu from "./components/selected_menu.js"; //fixed
 import updateCopyrightYear from "./helpers/date_updater.js"; //fixed
-import resetToHome from "./helpers/reload_page.js";
+import resetToHome from "./helpers/reload_page.js"; //working
 import sendEmail from "./helpers/send__form.js";
 
 parallax()
