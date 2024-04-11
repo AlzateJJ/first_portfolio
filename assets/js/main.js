2feffdbf331abacd8b7475cae2225762a2f2459c
AlzateJJ <124931160+AlzateJJ@users.noreply.github.com>
2024-04-11 14:31:42 -0500

    adding cv

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,7 @@ import parallax from "./components/parallax.js"; //fixed
 import activeMenu from "./components/selected_menu.js"; //fixed
 import updateCopyrightYear from "./helpers/date_updater.js"; //fixed
 import resetToHome from "./helpers/reload_page.js"; //working
-import sendEmail from "./helpers/send__form.js";
-import descargarArchivo from './helpers/download_cv.js'
+import sendEmail from "./helpers/send__form.js"; //fixed
 
 parallax()
 
@@ -14,5 +13,3 @@ resetToHome()
 updateCopyrightYear()
 
 sendEmail()
-
-descargarArchivo()
