eca5fde37eb469ff4b64cc7d328afce3c64aa6d4
AlzateJJ <124931160+AlzateJJ@users.noreply.github.com>
2024-04-10 20:19:58 -0500

    cv btn functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ import activeMenu from "./components/selected_menu.js"; //fixed
 import updateCopyrightYear from "./helpers/date_updater.js"; //fixed
 import resetToHome from "./helpers/reload_page.js"; //working
 import sendEmail from "./helpers/send__form.js";
+import descargarArchivo from './helpers/download_cv.js'
 
 parallax()
 
@@ -13,3 +14,5 @@ resetToHome()
 updateCopyrightYear()
 
 sendEmail()
+
+descargarArchivo()
