eedf4ef5e40dd580fbefdce4e86ccd06c1771310
AlzateJJ <124931160+AlzateJJ@users.noreply.github.com>
2024-04-12 14:22:20 -0500

    added langs and stuff

--- assets/js/helpers/reload_page.js
+++ assets/js/helpers/reload_page.js
@@ -1,5 +1,9 @@
 function resetToHome() {
     window.location.hash = '#home'
+
+    document.addEventListener('DOMContentLoaded', function () {
+        document.getElementById('c6').checked = true;
+    });
 }
 
 export default resetToHome
